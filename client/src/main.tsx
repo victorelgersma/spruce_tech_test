516290ae58bf35afff106124ddf2f6d8c8f2ddcd
Victor Elgersma <victor.elgersma41@thg.com>
2025-06-14 12:03:02 +0100

    wip: logging out which row and column was pressed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,12 +8,17 @@ export const Main = () => {
     [undefined, undefined, undefined],
     [undefined, undefined, undefined]
   ])
+  const [nextPlayer, setNextPlayer] = useState<XorO>('X')
 
+  const handleClick = ({rowIndex, columnIndex}) => {
+    console.log({ rowIndex, columnIndex });
+  }
   return <div className='flex flex-col mt-10 items-center gap-10'>
     <div className='font-bold text-2xl'>Tic Tac Toe</div>
+    <p> Next player to play: X</p>
     <div className='flex flex-col gap-1'>
-      {board.map(row => <div className='flex gap-1'>
-        {row.map(column => <div className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex'>
+      {board.map((row, rowIndex) => <div className='flex gap-1'>
+        {row.map((column, columnIndex) => <div className='flex border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold' onClick={() => handleClick({ rowIndex, columnIndex })}>
           {column}
         </div>)}
       </div>)}
